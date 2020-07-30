f90c2e3a4408407baf1ff0654575befc3934819d
Sergio Andre Rivas Medina <srivasme@everis.com>
2020-07-30 14:14:24 -0500

    :tada: Initial commit

---
+++ src/test/java/everis/bootcamp/atm/AtmApplicationTests.java
@@ -0,0 +1,13 @@
+package everis.bootcamp.atm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
